2ca1011b4e0908bea4c515d9bb9344245bf32d1f
Lee Parks <lcp3.dev@gmail.com>
2021-01-24 18:55:05 -0500

    Initial Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal"); //Axis case-sensitive, goes from -1 to 1 from left to right, middle/resting is 0
+        var rigidbody2d = GetComponent<Rigidbody2D>();
+        rigidbody2d.velocity = new Vector2(horizontal, rigidbody2d.velocity.y);
+    }
+}
