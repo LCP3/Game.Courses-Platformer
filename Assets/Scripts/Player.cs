e1f3cc0e0302d2c483a5f38a7ec233a758893f54
Lee Parks <lcp3.dev@gmail.com>
2021-01-25 15:29:37 -0500

    Added variable speed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float _speed = 1;
 
-    // Update is called once per frame
     void Update()
     {
-        var horizontal = Input.GetAxis("Horizontal"); //Axis case-sensitive, goes from -1 to 1 from left to right, middle/resting is 0
+        var horizontal = Input.GetAxis("Horizontal") * _speed; //Axis case-sensitive, goes from -1 to 1 from left to right, middle/resting is 0
         var rigidbody2d = GetComponent<Rigidbody2D>();
         rigidbody2d.velocity = new Vector2(horizontal, rigidbody2d.velocity.y);
     }
