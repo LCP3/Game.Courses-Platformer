d7e00d27f27486d0b3d4c80e2545cb802323cba3
Lee Parks <lcp3.dev@gmail.com>
2021-01-25 16:35:17 -0500

    Added sprite flip based on direction, basic jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@
 public class Player : MonoBehaviour
 {
     [SerializeField] float _speed = 1;
+    [SerializeField] float _jumpStrength = 200;
 
     void Update()
     {
@@ -11,10 +12,18 @@ public class Player : MonoBehaviour
         rigidbody2d.velocity = new Vector2(horizontal, rigidbody2d.velocity.y);
 
         var animator = GetComponent<Animator>(); //Get the Animator component
-        bool walking = horizontal != 0;
-        animator.SetBool("Walk", walking);
+        bool walking = horizontal != 0; //If our horizontal speed isn't 0
+        animator.SetBool("Walk", walking); // Walk
 
 
+        if (horizontal != 0) {
+            var spriteRenderer = GetComponent<SpriteRenderer>(); //Get the Sprite Renderer component
+            spriteRenderer.flipX = horizontal < 0; //            
+        }
+
+        if (Input.GetButtonDown("Fire1")) {
+            rigidbody2d.AddForce(Vector2.up * _jumpStrength);
+        }
 
     }
 }
