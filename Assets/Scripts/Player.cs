edd45561d36c3d148a7dfdc4fa540af61d756a5d
Lee Parks <lcp3.dev@gmail.com>
2021-01-28 20:45:37 -0500

    Added ground collision detection object for jump resetting purposes, added Player/Prop layers to filter which game objects are considered ground.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,17 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float _speed = 1;
     [SerializeField] float _jumpStrength = 200;
+    [SerializeField] int _maxJumps = 2;
+    [SerializeField] Transform _feet;
+
     Vector3 _startPosition;
+    int _jumpsRemaining;
+
 
     void Start()
     {
         _startPosition = transform.position;
+        _jumpsRemaining = _maxJumps;
     }
 
     void Update()
@@ -31,10 +37,19 @@ public class Player : MonoBehaviour
             spriteRenderer.flipX = horizontal < 0; //            
         }
 
-        if (Input.GetButtonDown("Fire1")) {
+        if (Input.GetButtonDown("Fire1") && _jumpsRemaining > 0) { // Square button
             rigidbody2d.AddForce(Vector2.up * _jumpStrength);
+            _jumpsRemaining--;
         }
 
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        var hit = Physics2D.OverlapCircle(_feet.position, 0.1f, LayerMask.GetMask("Default")); //Create overlapping circle to detect if we're grounded (point, radius(f for float), layer mask)
+        if (hit != null) { //If collision was successful
+            _jumpsRemaining = _maxJumps; // Reset Jumps
+        }          
     }
 
     internal void ResetToStart() //Internal means we can call this method from outside their own script (basically same as public)
