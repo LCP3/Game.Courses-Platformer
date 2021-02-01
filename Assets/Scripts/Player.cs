7aa6d2d6eceff2c6701ed7123582fbd7cb5f42fd
Lee Parks <lcp3.dev@gmail.com>
2021-02-01 17:11:40 -0500

    Changed jumping mechanics from a force addition to velocity, added code for a downward pull after jumping, added a jump duration (long press = higher jumps)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,13 +3,17 @@
 public class Player : MonoBehaviour
 {
     [SerializeField] float _speed = 1;
-    [SerializeField] float _jumpStrength = 200;
+    [SerializeField] float _jumpVelocity = 10;
     [SerializeField] int _maxJumps = 2;
     [SerializeField] Transform _feet;
+    [SerializeField] float _downPull = 5;
+    [SerializeField] float _maxJumpDuration = 0.1f;
 
     Vector3 _startPosition;
     int _jumpsRemaining;
-
+    float _fallTimer;
+    float _jumpTimer;
+    
 
     void Start()
     {
@@ -19,6 +23,10 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        var hit = Physics2D.OverlapCircle(_feet.position, 0.1f, LayerMask.GetMask("Default")); //Create overlapping circle to detect if we're grounded (point, radius(f for float), layer mask)
+        bool isGrounded = hit != null; //if hit = null, false | if != null, true (Are we grounded?)
+
+
         var horizontal = Input.GetAxis("Horizontal") * _speed; //Axis case-sensitive, goes from -1 to 1 from left to right, middle/resting is 0
         var rigidbody2d = GetComponent<Rigidbody2D>();
 
@@ -37,19 +45,30 @@ public class Player : MonoBehaviour
             spriteRenderer.flipX = horizontal < 0; //            
         }
 
-        if (Input.GetButtonDown("Fire1") && _jumpsRemaining > 0) { // Square button
-            rigidbody2d.AddForce(Vector2.up * _jumpStrength);
-            _jumpsRemaining--;
+        if (Input.GetButtonDown("Fire1") && _jumpsRemaining > 0)
+        { // JUMP
+            rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, _jumpVelocity); //Add Y velocity Vector2(x,y)
+            _jumpsRemaining--; //Decrease jump count
+            _fallTimer = 0; //Reset the fall timer
+            _jumpTimer = 0; //Reset jump timer
+        }
+        else if (Input.GetButton("Fire1") && _jumpTimer <= _maxJumpDuration && _jumpsRemaining > 0) { //If Jump is held
+            rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, _jumpVelocity); //Add Y velocity Vector2(x,y)
+            _fallTimer = 0; //Reset the fall timer
+            _jumpTimer += Time.deltaTime;
         }
 
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        var hit = Physics2D.OverlapCircle(_feet.position, 0.1f, LayerMask.GetMask("Default")); //Create overlapping circle to detect if we're grounded (point, radius(f for float), layer mask)
-        if (hit != null) { //If collision was successful
+        if (isGrounded) { // If we're grounded
+            _fallTimer = 0; //Reset the fall timer
             _jumpsRemaining = _maxJumps; // Reset Jumps
-        }          
+        }
+        else { //If we're NOT grounded
+            //Fall down in increasing velocity
+            _fallTimer += Time.deltaTime;
+            var downForce = _downPull * _fallTimer * _fallTimer;
+            rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, rigidbody2d.velocity.y - downForce);
+        }
+
     }
 
     internal void ResetToStart() //Internal means we can call this method from outside their own script (basically same as public)
