bdba453dfea816babd8f5cd06da54ac12728cc3d
Lee Parks <lcp3.dev@gmail.com>
2021-02-02 13:35:01 -0500

    Added flag objective + animation

---
+++ Assets/Scripts/Flag.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flag : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>(); //Get the Player component
+
+        if (player == null) { //If player isn't the collider
+            return; //Exit early
+        }
+
+        var animator = GetComponent<Animator>(); //Get the Animator
+        animator.SetTrigger("Raise"); //Trigger the Raise animation
+        //Play flag waving
+        //Load new level
+    }
+}
