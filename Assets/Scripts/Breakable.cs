c3125c0edd8a6b4b68d85015e966ca7e23c4a59b
Lee Parks <lcp3.dev@gmail.com>
2021-02-09 12:40:52 -0500

    Added breakable brick + script

---
+++ Assets/Scripts/Breakable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Breakable : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.GetComponent<Player>() == null) 
+        {
+            return;
+        }
+
+        if (collision.contacts[0].normal.y > 0) //If hit by player from below
+        {
+            TakeHit();
+        }
+    }
+
+    void TakeHit()
+    {
+        gameObject.SetActive(false);
+    }
+}
