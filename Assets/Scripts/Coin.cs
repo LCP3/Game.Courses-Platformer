62bd19b2ce0a916ff6d56e8808f92b94800f6871
Lee Parks <lcp3.dev@gmail.com>
2021-02-10 15:52:55 -0500

    Added a coin with a static counter.

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    static int _coinsCollected; //Static = only one instance of this variable across all game objects
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+
+        if (player == null) {
+            return;
+        }
+
+        gameObject.SetActive(false); //Turn off coin
+        _coinsCollected++; //Add to coin counter
+        Debug.Log(_coinsCollected);
+    }
+}
