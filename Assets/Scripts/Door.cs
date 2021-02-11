6f5b9c221f05763e5a59f4d5603205bfc4054f21
Lee Parks <lcp3.dev@gmail.com>
2021-02-10 20:35:04 -0500

    Added door that opens with debug (context) menu

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] Sprite _openMid;
+    [SerializeField] Sprite _openTop;
+    [SerializeField] SpriteRenderer _rendererMid;
+    [SerializeField] SpriteRenderer _rendererTop;
+
+    [ContextMenu("Open Door")]
+    void Open() //When our door opens
+    {
+        _rendererMid.sprite = _openMid; //Change out the sprites
+        _rendererTop.sprite = _openTop;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
