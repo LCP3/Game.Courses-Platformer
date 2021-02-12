fb0e16f19bc4cbd077d4ade8ed02fbfe25c02936
Lee Parks <lcp3.dev@gmail.com>
2021-02-11 21:31:06 -0500

    Added beginnings of falling platform

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    public bool PlayerTrigger;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        if (player == null)
+        {
+            return;
+        }
+
+        PlayerTrigger = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        if (player == null)
+        {
+            return;
+        }
+
+        PlayerTrigger = false;
+    }
+}
