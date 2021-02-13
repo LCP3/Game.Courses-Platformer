d36b5f0d4ce2f46484ea873a6eccef4a604a7e0d
Lee Parks <lcp3.dev@gmail.com>
2021-02-12 19:25:27 -0500

    Finished off base functionality for wiggling platform that falls

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class FallingPlatform : MonoBehaviour
 {
+    [SerializeField] float _fallSpeed = 3;
     public bool PlayerTrigger;
     
     HashSet<Player> _playersInTrigger = new HashSet<Player>(); //HashSet requires SysCol, allows us to have an instance of each player
     Vector3 _initialPosition;
+    bool _falling;
 
     void Start()
     {
@@ -40,24 +42,44 @@ public class FallingPlatform : MonoBehaviour
         Debug.Log("Wiggling");
         float wiggleTimer = 0;
 
-        while (wiggleTimer < 1)
+        while (wiggleTimer < 1f) //While we're wiggling
         {
-            float randomY = UnityEngine.Random.Range(-0.01f, 0.01f);
-            float randomX = UnityEngine.Random.Range(-0.01f, 0.01f);
+            float randomY = UnityEngine.Random.Range(-0.05f, 0.05f);
+            float randomX = UnityEngine.Random.Range(-0.05f, 0.05f);
 
-            transform.position = _initialPosition + new Vector3(randomX, randomY);
+            transform.position = _initialPosition + new Vector3(randomX, randomY); //Move our position a random distance from our initial position
             float randomDelay = UnityEngine.Random.Range(0.005f, 0.01f);
-            yield return new WaitForSeconds(randomDelay);
-            wiggleTimer += randomDelay;
+            yield return new WaitForSeconds(randomDelay); //Wait for a random delay
+            wiggleTimer += randomDelay; //Add to our wiggle timer
         }
 
         Debug.Log("Falling");
-        yield return new WaitForSeconds(3f);
+        _falling = true;
+
+        foreach (var collider in GetComponents<Collider2D>()) //Get our colliders into an array, also can be written "Colliders[] collider ="
+        {
+            collider.enabled = false; //Turn off our colliders in the array
+        }
+
+        float fallTimer = 0;
+
+        while (fallTimer < 3f)
+        {
+            transform.position += Vector3.down * Time.deltaTime* _fallSpeed; //Move down over time multiplied by our fall speed
+            fallTimer += Time.deltaTime;
+            Debug.Log(fallTimer);
+            yield return null; //Wait until next frame           
+        }
+        Destroy(gameObject); //Destroy our platform after looping complete
 
     }
 
     private void OnTriggerExit2D(Collider2D collision) //On player exiting the platform trigger
     {
+        if (_falling) //If we're falling, exit
+        {
+            return;
+        }
         var player = collision.GetComponent<Player>();
         if (player == null)
         {
