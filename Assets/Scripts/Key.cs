c3796277c973eaf11351eb4b953e60dfaa4ca3d7
Lee Parks <lcp3.dev@gmail.com>
2021-02-15 02:46:42 -0500

    Added lock that is associated with the yellow key.  Red key for testing invalid key.

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+    [SerializeField] KeyLock _keyLock;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         var player = collision.GetComponent<Player>();
@@ -11,6 +13,13 @@ public class Key : MonoBehaviour
         {
             transform.SetParent(player.transform);
             transform.localPosition = Vector3.up; // (0,1,0)
+        }
+
+        var keyLock = collision.GetComponent<KeyLock>();
+        if (keyLock != null && keyLock == _keyLock)
+        {
+            keyLock.Unlock(); //Run public Unlock() in KeyLock.cs
+            Destroy(gameObject); //Destroy our key after use
         }
     }
 }
