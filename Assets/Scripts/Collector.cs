5897a3ba8f27df8c369ee36dd9f23e5eb7237793
Lee Parks <lcp3.dev@gmail.com>
2021-02-16 02:06:15 -0500

    Added UI element for our collector

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -1,27 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class Collector : MonoBehaviour
 {
 
     [SerializeField] Collectible[] _collectibles;
+    TMP_Text _remainingCounter;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _remainingCounter = GetComponentInChildren<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        int countRemaining = 0; //
         foreach (var collectible in _collectibles)
         {
             if (collectible.isActiveAndEnabled)
             {
-                return;
+                countRemaining++;
             }
+        }
+
+        _remainingCounter?.SetText(countRemaining.ToString()); //Set the counter's text to our remaining amount of collectibles -- the ? is shorthand for != null (evaluates true)
+
+        if (countRemaining > 0)
+        {
+            return;
         }
         //if (_collectibles.Any(t => t.gameObject.activeSelf == true)) //Any uses System.Linq, if any item in the array matches the statement, if any items in the array return true, this statement will also return true
         //{
