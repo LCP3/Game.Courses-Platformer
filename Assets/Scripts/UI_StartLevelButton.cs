e1c23a486c694c21e6d8b6122826bdc82edec385
Lee Parks <lcp3.dev@gmail.com>
2021-02-27 01:39:59 -0500

    Added a New Game button with a main menu that loads Level 1.

---
+++ Assets/Scripts/UI_StartLevelButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_StartLevelButton : MonoBehaviour
+{
+    [SerializeField] string _levelName;
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(_levelName); // Load a level defined in the inspector
+    }
+}
