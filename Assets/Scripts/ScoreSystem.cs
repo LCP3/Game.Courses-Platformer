c09c37acb3ffacbdcf9216abc12b35d755651de3
Lee Parks <lcp3.dev@gmail.com>
2021-03-02 17:22:32 -0500

    Added a functional high-score system on the main menu.

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -6,11 +6,20 @@ public static class ScoreSystem
 {
     public static event Action<int> OnScoreChange;
     static int _score;
+    static int _highScore;
 
     public static void Add(int points)
     {
         _score += points;
         OnScoreChange?.Invoke(_score);
-        Debug.Log($"Score = {_score}");
+
+
+
+        if (_score > _highScore) // If the score is higher than the current high score
+        {
+            _highScore = _score; // Set new high score
+
+            PlayerPrefs.SetInt("highScore", _highScore); // Set a key in PlayerPrefs for a persistent high score.
+        }
     }
 }
---
+++ Assets/Scripts/UIHighScore.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class UIHighScore : MonoBehaviour
+{
+    TMP_Text _text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _text = GetComponent<TMP_Text>();
+        int _highScore = PlayerPrefs.GetInt("highScore");
+
+        _text.SetText("High Score: " + _highScore.ToString());
+    }
+
+    [ContextMenu("Reset High Score")]
+    public void ResetHighScore()
+    {
+        PlayerPrefs.DeleteKey("highScore");
+    }
+}
