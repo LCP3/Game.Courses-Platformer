7a89cd8d42df8ccf31d3ffaa81f3fb1ba600e145
Lee Parks <lcp3.dev@gmail.com>
2021-03-02 10:12:40 -0500

    Created an in-game score element

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -1,12 +1,16 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+
 
 public static class ScoreSystem
 {
+    public static event Action<int> OnScoreChange;
     static int _score;
 
     public static void Add(int points)
     {
         _score += points;
+        OnScoreChange?.Invoke(_score);
         Debug.Log($"Score = {_score}");
     }
 }
---
+++ Assets/Scripts/UIScore.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class UIScore : MonoBehaviour
+{
+    TMP_Text _text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _text = GetComponent<TMP_Text>();
+        ScoreSystem.OnScoreChange += UpdateScoreText; //When score changes, update the score text
+    }
+
+    private void UpdateScoreText(int score)
+    {
+        _text.SetText(score.ToString()); //Set text to score value
+    }
+}
