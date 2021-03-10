33be19e871f7ca0b9a49516fd9471faecc162f2a
Lee Parks <lcp3.dev@gmail.com>
2021-03-10 16:58:21 -0500

    Added a basic fireball and script to shoot fireballs attached to the player

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    [SerializeField] float _launchForce = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(_launchForce, 0);
+    }
+
+}
